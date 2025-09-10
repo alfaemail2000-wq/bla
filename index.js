63adc5962b5310344306f776f114c56ee0f909b2
C.Vadym <.Vadym>
2025-09-10 11:56:07 +0200

    Projekt initialisiert

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const sendRequest = () => {
+
+// 1: Anlegen des XMLHttpRequest-Objektes
+const xhr = new XMLHttpRequest();
+
+// 2: Definieren, was wie geladen werden soll
+xhr.open('get', '/data.txt');
+
+// 3: Definieren, was nach dem Laden geschehen soll
+xhr.addEventListener('load', () => {
+if (xhr.status == 200) 
+    
+    {
+    console.log(xhr)
+console.log(xhr.response);
+} else {
+console.warn(`${xhr.responseURL} konnte nicht geladen werden: ${xhr.statusText}`); 
+}
+})
+
+// 4: Absenden
+xhr.send();
+
+}
+
+const init = () => {
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', sendRequest);
+
+}
+
+init();
